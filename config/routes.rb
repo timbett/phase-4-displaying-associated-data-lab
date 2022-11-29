cf213e4d5653ff617534637f98018f139e7da5ed
Timothy Kibet <tkkibet13@gmail.com>
2022-11-29 05:34:47 +0300

    Assignment Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
+
+  get '/items', to: 'items#index'
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
 end
